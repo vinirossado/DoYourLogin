97a3fab15c713faa394e9be769c087468d9378d0
Vinicius Rossado <vinirossado@gmail.com>
2023-05-24 19:08:38 +0300

    Removing useless Test

--- source/utils/mapper_test.go
+++ source/utils/mapper_test.go
@@ -1,12 +1,5 @@
 package utils
 
-import (
-	"doYourLogin/source/domain/entities"
-	"doYourLogin/source/domain/enumerations"
-	"doYourLogin/source/domain/requests"
-	"testing"
-)
-
 type Source struct {
 	Field1 string
 	Field2 int
@@ -17,22 +10,22 @@ type Destination struct {
 	FieldB int
 }
 
-func TestMap(test *testing.T) {
-
-	user := requests.UserRequest{
-		ID:       4,
-		Name:     "ABC",
-		Username: "user1",
-		Email:    "dycjh@example.com",
-		Address:  "address1",
-		About:    "about1",
-		Password: "password1",
-		Image:    "image1",
-		Phone:    "phone1",
-		Role:     enumerations.GOD,
-	}
-
-	dto := entities.User{}
-
-	Map(user, &dto)
-}
+//func TestMap(test *testing.T) {
+//	//
+//	//user := requests.UserRequest{
+//	//	ID:       4,
+//	//	Name:     "ABC",
+//	//	Username: "user1",
+//	//	Email:    "dycjh@example.com",
+//	//	Address:  "address1",
+//	//	About:    "about1",
+//	//	Password: "password1",
+//	//	Image:    "image1",
+//	//	Phone:    "phone1",
+//	//	Role:     enumerations.GOD,
+//	//}
+//	//
+//	//dto := entities.User{}
+//	//
+//	//Map(user, &dto)
+//}
