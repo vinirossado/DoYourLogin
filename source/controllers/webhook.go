3e80b459d8d084ea89944575b423e2c57160315e
Vinicius Rossado <vinirossado@gmail.com>
2023-08-01 17:23:22 +0300

    Improvement webhook

--- source/controllers/webhook.go
+++ source/controllers/webhook.go
@@ -6,14 +6,37 @@ import (
 	"net/http"
 )
 
-type PostData struct {
-	Action     any `json:"action"`
-	Membership any `json:"membership"`
+type GithubWebhookUserManagePayload struct {
+	Action string `json:"action"`
+	Scope  string `json:"scope"`
+
+	Member struct {
+		ID    uint   `json:"id"`
+		Login string `json:"login"`
+		Type  string `json:"type"`
+	}
+
+	Sender struct {
+		ID    uint   `json:"id"`
+		Login string `json:"login"`
+		Type  string `json:"type"`
+	}
+
+	Team struct {
+		ID   uint   `json:"id"`
+		Name string `json:"name"`
+		Slug string `json:"slug"`
+	}
+
+	Organization struct {
+		ID    uint   `json:"id"`
+		Login string `json:"login"`
+		URL   string `json:"url"`
+	}
 }
 
 func HandlePost(c *gin.Context) {
-
-	var postData PostData
+	var postData GithubWebhookUserManagePayload
 	if err := c.ShouldBindJSON(&postData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
